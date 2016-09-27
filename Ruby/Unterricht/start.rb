cf4a38301f5a50f1b3819f6fafbd3a1f1f576ae3
g16bogner <g16bogner@gmail.com>
2016-09-27 10:34:58 +0200

    classes

--- Ruby/Unterricht/start.rb
+++ Ruby/Unterricht/start.rb
@@ -1,10 +1,21 @@
-def movie_listing(movie, rank=5) 
-	"Movie: #{movie} ist ein guter Film, mit der Punktzahl: #{rank} am #{set_time}"
+class Movie
+	def initialize(pname, prank=5)
+		@name = pname
+		@rank = prank
+	end
+
+	def list_movie
+		"Movie heisst: #{@name} und hat einen Rank von: #{@rank}"
+	end
+
+	def thumbs_up(value)
+		@rank = @rank + value
+	end
 end
 
-def set_time
-	Time.now.strftime("%d.%m.%Y")
-end
-
-puts movie_listing("Superman")
-
+obj = Movie.new("Superman")
+puts obj.list_movie
+obj.thumbs_up(7)
+puts obj.list_movie
+obj2 = Movie.new("Batman", 10)
+puts obj2.list_movie
