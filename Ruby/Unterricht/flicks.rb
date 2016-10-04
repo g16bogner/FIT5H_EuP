c41094f65ec3c8856b06e7057a8a53a6ba01f113
g16bogner <g16bogner@gmail.com>
2016-10-04 12:45:53 +0200

    array

--- Ruby/Unterricht/flicks.rb
+++ Ruby/Unterricht/flicks.rb
@@ -14,18 +14,14 @@ class Playlist
   end
 end
 class Movie
-  
   attr_accessor :title
-
   def initialize(title, rank=0)
     @title = title.capitalize
     @rank = rank
   end
-  
   def thumbs_down
     @rank -= 1
   end
-  
   def to_s
     "#{@title} has a rank of #{@rank}."
   end
