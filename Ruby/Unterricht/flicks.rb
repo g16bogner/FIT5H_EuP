7d7cfc4ba31e3cd7e2b1de2886543d452a41c49e
g16bogner <g16bogner@gmail.com>
2016-10-11 12:06:02 +0200

    hashes

--- Ruby/Unterricht/flicks.rb
+++ Ruby/Unterricht/flicks.rb
@@ -16,8 +16,16 @@ end
 class Movie
   attr_accessor :title
   def initialize(title, rank=0)
-    @title = title.capitalize
+    @title = capitalize_title(title)
     @rank = rank
+  end
+  def capitalize_title(str)
+    str_array = str.split
+    ret = ""
+    str_array.each do |teilstring|
+      ret = ret + " "  + teilstring.capitalize
+    end
+    ret.strip
   end
   def thumbs_down
     @rank -= 1
@@ -27,11 +35,12 @@ class Movie
   end
 end
 
-movie1 = Movie.new("Goonies", 10)
-movie2 = Movie.new("Batman", 5)
-movie3 = Movie.new("Spiderman", 8)
+movie1 = Movie.new("Goonies super", 10)
+puts movie1.title
+#movie2 = Movie.new("Batman", 5)
+#movie3 = Movie.new("Spiderman", 8)
 
-playlist1 = Playlist.new("Theo")
-playlist1.add_movie(movie1)
-playlist1.add_movie(movie2)
-playlist1.play
+#playlist1 = Playlist.new("Theo")
+#playlist1.add_movie(movie1)
+#playlist1.add_movie(movie2)
+#playlist1.play
