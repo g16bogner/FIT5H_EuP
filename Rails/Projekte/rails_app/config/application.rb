a152507143f2b304da01e09cee283b43fc3d9ae5
g16bogner <g16bogner@gmail.com>
2016-12-06 10:44:34 +0100

    show

--- Rails/Projekte/rails_app/config/application.rb
+++ Rails/Projekte/rails_app/config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module RailsApp
   class Application < Rails::Application
-  	config.i18n.available_locales = ["de_DE", :en]
+  	config.i18n.available_locales = [:de, :en]
     config.time_zone = "Berlin"
-    config.i18n.default_locale = :"de_DE"
+    config.i18n.default_locale = :de
     #config.i18n.enforce_available_locales = false
 	#config.i18n.available_locales = ["de_DE", :en]
 	#config.i18n.default_locale = :'de_DE'
--- Rails/Projekte/rails_app/config/routes.rb
+++ Rails/Projekte/rails_app/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "events" => "events#index"
-  get "events/:id" => "events#show"
-
-  #resources :events
+  resources :events
 end
