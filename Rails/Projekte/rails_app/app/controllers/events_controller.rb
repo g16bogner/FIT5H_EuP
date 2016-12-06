8442d4d828f6fa1d3eb3bc3eec8cb693bd78bab8
g16bogner <g16bogner@gmail.com>
2016-12-06 12:02:27 +0100

    form

--- Rails/Projekte/rails_app/app/controllers/events_controller.rb
+++ Rails/Projekte/rails_app/app/controllers/events_controller.rb
@@ -1,13 +1,38 @@
 class EventsController < ApplicationController
+	before_action :set_event, only: [:show, :edit, :update]
 	def index
 		@events = Event.all
 	end
 
 	def show
-		@event = Event.find(params[:id])
+		
 	end
 
 	def new
 		@event = Event.new
 	end
+
+	def create
+		@event = Event.create(event_params)
+		redirect_to events_path
+	end
+
+	def edit
+
+	end
+
+	def update
+
+		@event.update(event_params)
+		redirect_to events_path
+	end
+
+	private
+		def set_event
+			@event = Event.find(params[:id])
+		end
+
+		def event_params
+			params.require(:event).permit(:name, :description, :location, :price, :start_date)
+		end
 end
